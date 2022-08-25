1c10a3ed6511c28431ea1738d4b4c94606f9dcc6
AlicornMDH <75834074+cpony@users.noreply.github.com>
2022-08-25 22:43:44 +0800

    Add files via upload

---
+++ OpenGl/Project1/LightSpot.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <glm.hpp>
+//#include <gtc/matrix_transform.hpp>
+//#include <gtc/type_ptr.hpp>
+#include<gtx/rotate_vector.hpp>
+class LightSpot
+{
+public:
+	LightSpot(glm::vec3 _position, glm::vec3 _angles, glm::vec3 _color = glm::vec3(1.0f, 1.0f, 1.0f));
+	glm::vec3 position;
+	glm::vec3 angles;
+	glm::vec3 color;
+	glm::vec3 direction = glm::vec3(0, 0, 1.0f);
+	
+	void UpdateDirection();
+
+	float constant;
+	float linear;
+	float quadratic;
+
+	float cosPhyInner=0.9f;
+	float cosPhyOuter = 0.85f;
+
+
+};
+
